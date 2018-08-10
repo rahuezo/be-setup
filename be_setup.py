3a2ab3643a8f283711ad03efa6d27c6debb12bde
rhuezo <rudy.huezo@goexactly.com>
2018-08-10 10:11:16 -0700

    Finished BE Setup

--- be_setup.py
+++ be_setup.py
@@ -1,16 +1,20 @@
 import os, sys, time
 
+BASE_DIRS = [
+    'CSMS and Reports',
+    'Email Campaigns',
+    'Lead Projects'
+]
 
-DIRS = [
-    ['0 Resources', 'notes.txt' ],
-    '1 Intake Form',
-    '2 Research',
-    '3 Sales Navigator Company Parsing',
-    '4 Email Syntax',
-    '5 BE Ready Files',
-    '6 BEA Files',    
-    '7 Final Files LinkedIn',
-    '8 Final Files Sans LinkedIn',
+DIRS = [    
+    ['Lead Projects/{}/0 Resources', 'notes.txt' ],
+    'Lead Projects/{}/1 Intake Form',
+    'Lead Projects/{}/2 Research',
+    'Lead Projects/{}/3 Sales Navigator Company Parsing',
+    'Lead Projects/{}/4 Email Syntax',
+    'Lead Projects/{}/5 BE Ready Files',
+    'Lead Projects/{}/6 BEA Files',    
+    'Lead Projects/{}/7 Final Files LinkedIn',
 ]
 
 
@@ -22,13 +26,22 @@ if not project_name:
     print "\nNo project name given. Goodbye!\n"
     sys.exit()
 
+for base_directory in BASE_DIRS: 
+    dir_path = os.path.join(project_home, base_directory)
+
+    if not os.path.exists(dir_path): 
+        os.makedirs(dir_path)
+        print "Created {}".format(dir_path)
+    else:
+        print "{} already exists!".format(dir_path)
+
 home_path = os.path.join(project_home, project_name)
 
 for directory in DIRS:
     if type(directory) == list:
-        dir_path = os.path.join(home_path, directory[0])
+        dir_path = os.path.join(project_home, directory[0].format(project_name))
     else: 
-        dir_path = os.path.join(home_path, directory)
+        dir_path = os.path.join(project_home, directory.format(project_name))
 
     if not os.path.exists(dir_path): 
         os.makedirs(dir_path)
@@ -49,4 +62,4 @@ for directory in DIRS:
 
     time.sleep(0.2)
 
-os.startfile(home_path)
+os.startfile(os.path.join(project_home, 'Lead Projects', project_name))
